53def22f960c7ba6a1bc966af747c7d4ba6a5c59
Kamil Bęben <kbeben@puretech.com.pl>
2022-05-24 20:33:09 +0200

    CLIENT | Removed unnecessary navigation drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,44 +1,12 @@
 <template>
-  <v-app xdark>
-
-    <v-navigation-drawer
-      v-model="drawerIsVisible"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(drawerItem, index) in drawerItems"
-          :key="index"
-          :to="drawerItem.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ drawerItem.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="drawerItem.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+  <v-app dark>
 
     <v-app-bar
       fixed
       app
     >
-      
-      <v-app-bar-nav-icon
-        @click.stop="drawerIsVisible = !drawerIsVisible"
-      />
-      
-      <v-toolbar-title
-        v-text="title"
-      />
-
+      <v-toolbar-title v-text="$t('toolbar.title')"/>
       <v-spacer/>
-      
     </v-app-bar>
 
     <v-main>
@@ -56,27 +24,10 @@ import { Component, Vue } from 'nuxt-property-decorator'
 
 import '~/static/main.css'
 
-type DrawerItem = {
-  icon : string
-  title : string
-  to ?: string
-}
-
 @Component({
   name: 'DefaultLayout'
 })
 export default class DefaultLayout extends Vue {
-
-  private drawerIsVisible : boolean = false
-  private drawerItems : DrawerItem[] = [
-    {
-      icon: 'mdi-menu',
-      title: 'Demo',
-      to: '/type_script_demo'
-    }
-  ]
-
-  private title : string = this.$tc('toolbar.title')
 
   private mounted () {
     const previousWindowOnFocus = window.onfocus
